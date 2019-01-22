7929614a3b77c51d7ad18f3aa34220d9d119b287
haran19 <haranloga95@gmail.com>
2019-01-22 13:48:58 +0530

    59  commit chatsendingmessage

---
+++ app/src/main/java/com/example/haran/agritec/AgroServiceClickPostActivity.java
@@ -0,0 +1,155 @@
+package com.example.haran.agritec;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+public class AgroServiceClickPostActivity extends AppCompatActivity {
+
+    private ImageView postImage;
+    private TextView postDescription;
+    private Button DeletePostButton,EditPostButton;
+    private DatabaseReference ClickPostRef;
+
+    private FirebaseAuth mAuth;
+
+    private String PostKey, currentUserID ,databaseUserID ,description,image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agro_service_click_post);
+        mAuth=FirebaseAuth.getInstance();
+        currentUserID=mAuth.getCurrentUser().getUid();
+
+
+        PostKey=getIntent().getExtras().get("PostKey").toString();
+        ClickPostRef = FirebaseDatabase.getInstance().getReference().child("AgroService").child(PostKey);
+
+
+        postImage=(ImageView) findViewById(R.id.click_post_image);
+        postDescription=(TextView) findViewById(R.id.click_post_description);
+        DeletePostButton=(Button) findViewById(R.id.delete_post_button);
+        EditPostButton=(Button) findViewById(R.id.edit_post_button);
+
+        DeletePostButton.setVisibility(View.INVISIBLE);
+        EditPostButton.setVisibility(View.INVISIBLE);
+
+
+        ClickPostRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+
+                if(dataSnapshot.exists()) {
+                    description = dataSnapshot.child("description").getValue().toString();
+                    image = dataSnapshot.child("postimage").getValue().toString();
+
+                    databaseUserID = dataSnapshot.child("uid").getValue().toString();
+
+
+                    postDescription.setText(description);
+                    Picasso.get().load(image).into(postImage);
+
+
+                    if (currentUserID.equals(databaseUserID)) {
+
+                        DeletePostButton.setVisibility(View.VISIBLE);
+                        EditPostButton.setVisibility(View.VISIBLE);
+                    }
+
+                    EditPostButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v)
+                        {
+                            EditCurrentPost(description);
+
+
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+
+
+            }
+        });
+
+        DeletePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DeleteCurrentPost();
+            }
+        });
+
+    }
+
+    private void EditCurrentPost(String description)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(AgroServiceClickPostActivity.this);
+        builder.setTitle("Edit Post");
+
+        final EditText inputField = new EditText(AgroServiceClickPostActivity.this);
+        inputField.setText(description);
+        builder.setView(inputField);
+
+        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                ClickPostRef.child("description").setValue(inputField.getText().toString());
+                Toast.makeText(AgroServiceClickPostActivity.this,"Post has been updated successsfully",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.cancel();
+            }
+        });
+        Dialog dialog=builder.create();
+        dialog.show();
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.holo_green_dark);
+
+    }
+
+    private void DeleteCurrentPost()
+    {
+        ClickPostRef.removeValue();
+        SendUserToMainActivity();
+
+        Toast.makeText(this,"Post has been deleted",Toast.LENGTH_SHORT).show();
+
+    }
+    private void SendUserToMainActivity()
+    {
+        Intent mainIntent =new Intent(AgroServiceClickPostActivity.this,AgroServiceViewActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/haran/agritec/ChatActivity.java
+++ app/src/main/java/com/example/haran/agritec/ChatActivity.java
@@ -1,23 +1,34 @@
 package com.example.haran.agritec;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,15 +38,19 @@ public class ChatActivity extends AppCompatActivity {
     private ImageButton SendMessageButton,SendImagefileButton;
     private EditText userMessageInput;
     private RecyclerView userMessageList;
-    private String messageReceiverID,messageReceiverName;
+    private String messageReceiverID,messageReceiverName,messageSenderID, saveCurrentDate,saveCurrentTime;
     private TextView receiverName;
     private CircleImageView receiverProfileImage;
     private DatabaseReference  RootRef;
+    private FirebaseAuth mauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mauth =FirebaseAuth.getInstance();
+        messageSenderID= mauth.getCurrentUser().getUid();
 
         RootRef = FirebaseDatabase.getInstance().getReference();
 
@@ -45,6 +60,74 @@ public class ChatActivity extends AppCompatActivity {
 
         InitializerFields();
         DisplayReceiverInfo();
+
+        SendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+              SendMessage();
+            }
+        });
+    }
+
+    private void SendMessage() {
+
+        String messageText=userMessageInput.getText().toString();
+        if(TextUtils.isEmpty(messageText))
+        {
+            Toast.makeText(ChatActivity.this,"please type a message first",Toast.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+            String message_sender_ref="Messages/"+messageSenderID+"/"+messageReceiverID;
+            String message_receiver_ref="Messages/"+messageReceiverID+"/"+messageSenderID;
+
+            DatabaseReference user_message_key = RootRef.child("Messages").child(messageSenderID)
+                    .child(messageReceiverID).push();
+            String messgae_push_id=user_message_key.getKey();
+
+            Calendar calFordDate = Calendar.getInstance();
+            SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-YYYY");
+            saveCurrentDate = currentDate.format(calFordDate.getTime());
+
+            Calendar calFordTime = Calendar.getInstance();
+            SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm aa");
+            saveCurrentTime = currentTime.format(calFordDate.getTime());
+
+            Map messageTextBody = new HashMap();
+            messageTextBody.put("message",messageText);
+            messageTextBody.put("Time",saveCurrentTime);
+            messageTextBody.put("date",saveCurrentDate);
+            messageTextBody.put("type","text");
+            messageTextBody.put("from",messageSenderID);
+
+
+            Map messageBodyDetails = new HashMap();
+            messageBodyDetails.put(message_sender_ref+"/"+messgae_push_id , messageTextBody);
+            messageBodyDetails.put(message_receiver_ref+"/"+messgae_push_id , messageTextBody);
+
+            RootRef.updateChildren(messageBodyDetails).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(ChatActivity.this,"Message Send Successfully",Toast.LENGTH_SHORT).show();
+
+                    }
+                    else
+                        {
+                           String message=task.getException().getMessage();
+                            Toast.makeText(ChatActivity.this,"Eror"+message,Toast.LENGTH_SHORT).show();
+                        }
+                        userMessageInput.setText("");
+
+                }
+            });
+
+        }
+
     }
 
     private void DisplayReceiverInfo() {
@@ -87,7 +170,7 @@ public class ChatActivity extends AppCompatActivity {
         receiverName=(TextView) findViewById(R.id.custom_profile_name);
         receiverProfileImage = (CircleImageView)findViewById(R.id.custom_profile_image);
         SendMessageButton =(ImageButton)findViewById(R.id.send_message_button);
-        SendMessageButton =(ImageButton)findViewById(R.id.send_image_file_button);
+        SendImagefileButton=(ImageButton)findViewById(R.id.send_image_file_button);
         userMessageInput =(EditText) findViewById(R.id.input_message);
 
 
